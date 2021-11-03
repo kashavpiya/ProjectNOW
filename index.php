9c4aadb87ebf09b743bc3b030419dc5496d4d91f
kashavpiya <kashavpiya19@augustana.edu>
2021-11-03 16:15:23 -0500

    .

---
+++ index.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Home | Project NOW</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Karla:wght@200;300;400;500;600;700;800&display=swap"
+        rel="stylesheet">
+
+</head>
+
+<body>
+    
+<?php include('includes/header.html'); ?>
+
+        <div class="banner">
+            <div class="img banner-img">
+                <h1>Heading Goes Here</h1>
+                <h2>A short description can go here.</h2>
+                <a href="#info">Click Here</a>
+            </div>
+        </div>
+
+        <div class="button-grid">
+            <a href="#volunteer">Volunteer</a>
+            <a href="#donate">Donate</a>
+            <a href="#programs">Programs</a>
+        </div>
+
+        <div class="content">
+            <h2>PROJECT NOW PROGRAMS</h2>
+            <div class="content-grid">
+                <div class="textSide">
+                    <h3>Head Start</h3>
+                    <p>Write a short description of this program here. Write a short description of this program here.
+                    </p>
+                    <a href="#learn1">Learn More</a>
+                </div>
+                <div class="pictureSide">
+                    <img src="img/pic1.jpg" alt="Kids sitting down with all their legs together in a circle">
+                </div>
+                <div class="pictureSide hideMob">
+                    <img src="img/pinata.jpg" alt="Kids breaking a pinata">
+                </div>
+                <div class="textSide">
+                    <h3>Community Services</h3>
+                    <p>Write a short description of this program here. Write a short description of this program here.
+                    </p>
+                    <a href="#learn1">Learn More</a>
+                </div>
+                <div class="pictureSide mobOnly">
+                    <img src="img/pinata.jpg" alt="Kids breaking a pinata">
+                </div>
+                <div class="textSide">
+                    <h3>Housing</h3>
+                    <p>Write a short description of this program here. Write a short description of this program here.
+                    </p>
+                    <a href="#learn1">Learn More</a>
+                </div>
+                <div class="pictureSide">
+                    <img src="img/pic1.jpg" alt="Kids sitting down with all their legs together in a circle">
+                </div>
+                <div class="pictureSide hideMob">
+                    <img src="img/senior1.JPG" alt="Kids sitting down with all their legs together in a circle">
+                </div>
+                <div class="textSide">
+                    <h3>Senior Services</h3>
+                    <p>Write a short description of this program here. Write a short description of this program here.
+                    </p>
+                    <a href="#learn1">Learn More</a>
+                </div>
+                <div class="pictureSide mobOnly">
+                    <img src="img/senior1.JPG" alt="Kids sitting down with all their legs together in a circle">
+                </div>
+                <div class="textSide">
+                    <h3>Senior Services</h3>
+                    <p>Write a short description of this program here. Write a short description of this program here.
+                    </p>
+                    <a href="#learn1">Learn More</a>
+                </div>
+                <div class="pictureSide">
+                    <img src="img/pic1.jpg" alt="Kids sitting down with all their legs together in a circle">
+                </div>
+            </div>
+
+            <div class="report">
+                <img src="img/report.png" alt="report 2020">
+            </div>
+
+            <div class="bot-content-grid">
+                <div class="bot-left">
+                    <p class="mid leftleft">Serving Henry, Mercer,<br> and Rock Island Counties</p>
+                    <img src="img/comAct.png" alt="Helping people, changing lives">
+                </div>
+                <div class="bot-right">
+                    <p class="mid outreach">Outreach Officers:</p>
+                    <div class="bot-right-cont">
+                        <p class="address">418 19th St. Rock Island</p>
+                        <p class="phone">(309) 793-6391</p>
+                        <p class="address">1126 W. Mill St. Kewanee</p>
+                        <p class="phone">(309) 852-4565</p>
+                        <p class="address">605 1/2 SW 3rd St., Aledo</p>
+                        <p class="phone">(309) 582-2644</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <footer>
+            <div class="footNav">
+                <div class="home">
+                    <a href="index.php" class="footTitle" style="color: black; text-decoration: none;"><span
+                            class="mobOnly">&gt; </span>Home</a>
+                </div>
+                <div class="about">
+                    <a href="#about" class="footTitle" style="color: black; text-decoration: none;"><span
+                            class="mobOnly">&gt; </span>About</a><br class="hideMob">
+                    <a href="#sub-page" class="footContent hideMob"
+                        style="color: #A62035; text-decoration: none;">sub-page</a><br class="hideMob">
+                    <a href="#sub-page" class="footContent hideMob"
+                        style="color: #A62035; text-decoration: none;">sub-page</a><br class="hideMob">
+                </div>
+                <div class="partners">
+                    <a href="#partners" class="footTitle" style="color: black; text-decoration: none;"><span
+                            class="mobOnly">&gt; </span>Partners</a><br class="hideMob">
+                    <a href="#sub-page" class="footContent hideMob"
+                        style="color: #A62035; text-decoration: none;">sub-page</a><br class="hideMob">
+                    <a href="#sub-page" class="footContent hideMob"
+                        style="color: #A62035; text-decoration: none;">sub-page</a><br class="hideMob">
+                </div>
+                <div class="advocacy">
+                    <a href="#advocacy" class="footTitle" style="color: black; text-decoration: none;"><span
+                            class="mobOnly">&gt; </span>Advocacy</a><br class="hideMob">
+                    <a href="#sub-page" class="footContent hideMob"
+                        style="color: #A62035; text-decoration: none;">sub-page</a><br class="hideMob">
+                    <a href="#sub-page" class="footContent hideMob"
+                        style="color: #A62035; text-decoration: none;">sub-page</a><br class="hideMob">
+                    <a href="#sub-page" class="footContent hideMob"
+                        style="color: #A62035; text-decoration: none;">sub-page</a><br class="hideMob">
+                    <a href="#sub-page" class="footContent hideMob"
+                        style="color: #A62035; text-decoration: none;">sub-page</a><br class="hideMob">
+                </div>
+                <div class="services">
+                    <a href="#services" class="footTitle" style="color: black; text-decoration: none;"><span
+                            class="mobOnly">&gt; </span>Services</a>
+                </div>
+                <div class="impact">
+                    <a href="#impact" class="footTitle" style="color: black; text-decoration: none;"><span
+                            class="mobOnly">&gt; </span>Impact</a><br class="hideMob">
+                    <a href="#sub-page" class="footContent hideMob"
+                        style="color: #A62035; text-decoration: none;">sub-page</a><br class="hideMob">
+                    <a href="#sub-page" class="footContent hideMob"
+                        style="color: #A62035; text-decoration: none;">sub-page</a><br class="hideMob">
+                </div>
+                <div class="engagement">
+                    <a href="#engagement" class="footTitle" style="color: black; text-decoration: none;"><span
+                            class="mobOnly">&gt; </span>Engagement</a>
+                </div>
+
+            </div>
+            <div class="foot-logo logo hideMob">
+                <img src="img/logo.png">
+                <div class="logoName">
+                    <p class="title">Project NOW</p>
+                    <p class="subT">Community Action Agency</p>
+                </div>
+            </div>
+
+            <div class="mobOnly logoTx">
+                <a href="index.php" class="logoLink"><img src="img/logoTxts.png"
+                        alt="Project Now, Community Action Agency logo"></a>
+            </div>
+            <div class="creds">
+                <p> Copyright Project NOW <span class="hideMob">&nbsp; &nbsp; &nbsp;| &nbsp; &nbsp; &nbsp; </span><span
+                        class="mobOnly"><br></span>Site by Augustana Web Guild</p>
+            </div>
+        </footer>
+    </main>
+</body>
+<script>
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+</script>
+
+</html>
