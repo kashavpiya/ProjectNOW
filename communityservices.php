fffee9d6679bb6c12a86f82ecfe078f600de1faf
kashavpiya <kashavpiya19@augustana.edu>
2021-11-10 16:09:56 -0600

    service page

---
+++ communityservices.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Community Services | Project NOW</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Karla:wght@200;300;400;500;600;700;800&display=swap"
+        rel="stylesheet">
+        <script src="myscripts.js"></script>
+
+</head>
+
+<body>
+    <?php include('includes/header.html'); ?>
+        <div class="aboutpage">
+            <div class="left-content">
+                <h2>Community Services</h2>
+                <p>Project NOW provides a variety of services that help people with basic needs throughout Rock Island, Henry and Mercer counties. The Community Services Division provides application assistance, support, information and referral to the residents of these counties. Funded primarily by the Illinois Department of Commerce and Economic Opportunity, the Illinois Department of Human Services and the Illinois State Board of Education, customers are helped to manage their needs:</p>
+                <h3>Basic Needs Assistance:</h3>
+                <p>Project NOW offers a variety of programs designed to aid in personal and community economic development. These programs were developed with the assistance of Community Services Block Grant (CSBG) funds and are designed to help provide eligible customers with opportunities to increase their earning potential.</p>
+                <p>Project NOW has a long history of providing help to people who are struggling to meet very basic needs, including:
+                <ul>
+                    <li><a href="#utility">Utility Assistance</a></li><br>
+                    <li><a href="#rentandsecurity">Rent and Security Deposit Assistance</a></li><br>
+                    <li><a href="#food">Food and Nutrition Programs</a></li><br>
+                    <li><a href="#healthcare">Health Care Assistance and Referrals</a></li><br>
+                    <li><a href="#transport">Transportation Vouchers and Referrals</a></li><br>
+                    <li><a href="#cloth">Clothing Vouchers for Good Things NOW Clothing Stores</a></li><br>
+                    <li><a href="#household">Household Budgeting and Financial Literacy</a></li><br>
+                </ul></p>
+                <h3>Personal and Economic Development:</h3>
+                <p>Project NOW offers a variety of programs designed to aid in personal and community economic development. These programs were developed with the assistance of Community Services Block Grant (CSBG) funds and are designed to help provide eligible customers with opportunities to increase their earning potential.</p>
+                <p><strong>Scholarship Program</strong></p>
+                <p><a href="docs/scholarshipapp.pdf" target="_blank">Scholarship application (pdf)</a></p>
+            </div>
+            <div class="right-content">
+                <a href="communityservices.php">Community Services</a><br><br>
+                <a href="#eviction">Eviction Diversion Program</a><br><br>
+                <a href="#goodthings">Good Things NOW Store</a><br><br>
+                <a href="#headstart">Head Start</a><br><br>
+                <a href="#homeless">Homeless Services</a><br><br>
+                <a href="#housing">Housing Programs</a><br><br>
+                <a href="#property">Property Rentals</a><br><br>
+                <a href="https://www.ridetherim.org" target="_blank">RIM Rural Public Transit</a><br><br>
+                <a href="#senior">Senior Services</a><br><br>
+                <a href="#utility">Utility Assistance</a><br><br>
+                <a href="#weather">Weatherization</a>
+            </div>
+        </div>
+    <?php include('includes/footer.html'); ?>
+    </main>
+</body>
+</html>
