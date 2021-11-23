5a4a162fd12568ed46936d034f5f70a8cc4be392
kashavpiya <71453613+kashavpiya@users.noreply.github.com>
2021-11-23 15:26:03 -0600

    added rentals page

---
+++ rentals.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Property Rentals | Project NOW</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Karla:wght@200;300;400;500;600;700;800&display=swap"
+        rel="stylesheet">
+        <script src="myscripts.js"></script>
+
+</head>
+
+<body>
+    <?php include('includes/header.html'); ?>
+        <div class="aboutpage">
+            <div class="left-content">
+                <h2>Property Rentals</h2>
+                <p>Project NOW's Housing Division in its capacity as a Community Development Corporation (CDC) directs both single and multi-family rental property development. Project NOW has developed over 100 residential and commercial spaces serving people with low incomes throughout Rock Island, Henry and Mercer counties. Project NOW along with private and public sector partners has been developing rental properties for over 20 years.</p>
+                <p><a href="docs/Available-Apartments.pdf" target="_blank">See rental properties</a> | <a href="docs/rental-application.pdf" target="_blank">Download our rental application</a></p>
+                <p>Although tenant rents provide basic operating revenue, essential funding for maintenance and updates on Project NOW properties has been provided throughout the years from:</p>
+                <p>Doris and Victor Day Foundation | Amy Helpenstell Foundation | Moline Foundation | Rauch Family Foundation | John Deere Foundation | Community Foundation of the Great River Bend</p>
+                <h3 style="color: #0067b9;">Knox Apartments : Rents $340 - $480</h3>
+                <div class="apartment">
+                    <div style="text-align: center;">
+                        <img src="img/image_knox.jpg" alt="Knox Apartments" class="good1">
+                    </div>
+                    <p style="margin-top: 0;"><strong>1126 W.  Mill Street, Kewanee, IL</strong><br>
+                    1, 2, 3 and 4 Bedroom Units<br>
+                    Mix of Townhomes and Apartments<br>
+                    Newly Refurbished Inside and Out<br>
+                    Central Air<br>
+                    Lots of Closet Space<br>
+                    On-Site Laundry<br>
+                    Playground and Large Common Green Space<br>
+                    Community Room<br>
+                    Excellent Location<br>
+                    Off Street Parking<br>
+                    Some Units Are Handicap Accessible<br>
+                    Water, Sewer and Trash Pickup Included</p>
+                </div>
+                
+                <hr>
+                
+                <h3 style="color: #0067b9;">Old Chicago Apartments : Rents $385 - $465</h3>
+                <div class="apartment">
+                    <div style="text-align: center;">
+                        <img src="img/image_chicago.jpg" alt="Old Chicago Apartments" class="good1">
+                    </div>
+                    <p style="margin-top: 0;"><strong>1216 4th Avenue, Rock Island, IL</strong><br>
+                    	
+
+                        1,2 and 3 Bedroom Townhouse Apartments<br>
+                        Refurbished<br>
+                        Central Air Large Rooms with High Ceilings<br>
+                        Within Walking Distance to Downtown<br>
+                        Water, Sewer, Trash Pickup Included</p>
+                </div>
+                
+                <hr>
+                
+                <h3 style="color: #0067b9;">New Boston Apartments : Rents $300 - $405</h3>
+                <div class="apartment">
+                    <div style="text-align: center;">
+                        <img src="img/image_boston.jpg" alt="New Boston Apartments" class="good1">
+                    </div>
+                    <p style="margin-top: 0;"><strong>208 Walnut Street, New Boston, IL</strong><br>
+                    	
+
+                        1, 2, and 3 Bedroom Apartments<br>
+                        Quiet Setting<br>
+                        Central Air<br>
+                        Incredible Closet and Storage Space<br>
+                        Community Room and Playground<br>
+                        On-Site Laundry<br>
+                        Off Street Parking<br>
+                        Water, Sewer, Trash Pickup Included</p>
+                </div>
+                
+                <hr>
+                
+                <h3 style="color: #0067b9;">Marigold Apartments : Rents $300 - $375</h3>
+                <div class="apartment">
+                    <div style="text-align: center;">
+                        <img src="img/image_marigold.jpg" alt="Marigold Apartments" class="good1">
+                    </div>
+                    <p style="margin-top: 0;"><strong>Illinois Route 17 & 16th Avenue, Viola, IL</strong><br>
+                    	
+
+                        1 and 2 Bedroom Apartments<br>
+                        Quiet Senior Living<br>
+                        Heat Is Included in Rent<br>
+                        All Units Handicap Accessible or Adaptable<br>
+                        Recreation Room<br>
+                        Playground<br>
+                        On-Site Laundry<br>
+                        Off Street Parking<br>
+                        Close to Post Office and Convenience Store<br>
+                        Water, Sewer, Trash Pickup Included</p>
+                </div>
+                
+                <hr>
+                
+                <h3 style="color: #0067b9;">Rivercrest Apartments : Rents $375 - $425</h3>
+                <div class="apartment">
+                    <div style="text-align: center;">
+                        <img src="img/image_rivercrest.jpg" alt="Rivercrest Apartments" class="good1">
+                    </div>
+                    <p style="margin-top: 0;"><strong>1701 Washington Street, Keithsburg, IL</strong><br>
+                    	
+
+                        2 and 3 Bedroom Apartments<br>
+                        Quiet Rural Setting<br>
+                        Heat Is Included in Rent<br>
+                        All Units Handicap Accessible or Adaptable<br>
+                        Playground<br>
+                        On-Site Laundry<br>
+                        Off Street Parking<br>
+                        Water, Sewer, Trash Pickup Included</p>
+                </div>
+                
+                <hr>
+                
+                <h3 style="color: #0067b9;">Scattered Sites : Rents $325 - $525</h3>
+                <div class="apartment">
+                    <div style="text-align: center;">
+                        <img src="img/image_scattered1.jpg" alt="Scattered Apartments" class="good1">
+                        <img src="img/image_scattered2.jpg" alt="Scattered Apartments" class="good1">
+                    </div>
+                    <p style="margin-top: 0;">
+                    	
+
+                        	
+                        Project NOW Offers Affordable 1,2 and 3 bedroom units Located Throughout Rock Island, Mercer and Henry Counties<br>
+                        Scattered Sites 2 PhotoSingle Family Homes<br>
+                        Duplexes<br>
+                        Apartments<br>
+                        Sleeping Rooms<br>
+                        Downtown Apartments</p>
+                </div>
+                
+                <p>Utilities unless otherwise noted are the responsibility of the tenant.  To inquire about rental properties, please call (309) 793-6391 and ask for Property and Assets.</p>
+                
+            </div>
+            <div class="right-content">
+                <a href="communityservices.php">Community Services</a><br><br>
+                <a href="eviction.php">Eviction Diversion Program</a><br><br>
+                <a href="goodthings.php">Good Things NOW Store</a><br><br>
+                <a href="headstart.php">Head Start</a><br><br>
+                <a href="homelessservices.php">Homeless Services</a><br><br>
+                <a href="housing.php">Housing Programs</a><br><br>
+                <a href="rentals.php">Property Rentals</a><br><br>
+                <a href="https://www.ridetherim.org" target="_blank">RIM Rural Public Transit</a><br><br>
+                <a href="#senior">Senior Services</a><br><br>
+                <a href="#utility">Utility Assistance</a><br><br>
+                <a href="#weather">Weatherization</a>
+            </div>
+        </div>
+    <?php include('includes/footer.html'); ?>
+    </main>
+</body>
+</html>
